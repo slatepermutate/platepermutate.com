0ac0630fd33b317ce4cf2954f494779c0865de56
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2021-07-23 07:27:20 +0000

    Attempt to address document ready issue.

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-const images = [
-  'https://i.imgur.com/9en0it9.jpg',
-  'https://i.imgur.com/xtvFhVz.jpg',
-  'https://i.imgur.com/6xHWJaX.jpg',
-  'https://i.imgur.com/26VMISo.jpg',
-  'https://i.imgur.com/wuCdxHn.jpg',
-  'https://i.imgur.com/cDPHMm1.jpg',
-];
-let imageIndex = Date.now() % images.length;
-document.body.style.backgroundSize = 'cover';
-(function setBackground () {
-  document.body.style.backgroundImage = `url('${images[imageIndex = (imageIndex + 1) % images.length]}')`;
-  setTimeout(setBackground, 30000);
-})();
+ready(() => {
+  const images = [
+    'https://i.imgur.com/9en0it9.jpg',
+    'https://i.imgur.com/xtvFhVz.jpg',
+    'https://i.imgur.com/6xHWJaX.jpg',
+    'https://i.imgur.com/26VMISo.jpg',
+    'https://i.imgur.com/wuCdxHn.jpg',
+    'https://i.imgur.com/cDPHMm1.jpg',
+  ];
+  let imageIndex = Date.now() % images.length;
+  document.body.style.backgroundSize = 'cover';
+  (function setBackground () {
+    document.body.style.backgroundImage = `url('${images[imageIndex = (imageIndex + 1) % images.length]}')`;
+    setTimeout(setBackground, 30000);
+  })();
+});
+
+function ready(f) {
+  if (document.readyState === 'complete') {
+    Promise.resolve().then(f);
+  }
+  document.addEventListener('readystatechange', true, f);
+}
